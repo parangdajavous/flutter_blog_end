fdca0faa2f3fd231350edd0840e5dfb48a468e97
parangdajavous <parangdajavous@naver.com>
2025-06-05 17:01:51 +0900

    로그인 완료

--- lib/data/gvm/session_gvm.dart
+++ lib/data/gvm/session_gvm.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/utils/my_http.dart';
 import 'package:flutter_blog/data/repository/user_repository.dart';
 import 'package:flutter_blog/main.dart';
 import 'package:flutter_blog/ui/pages/auth/join_page/join_fm.dart';
+import 'package:flutter_blog/ui/pages/auth/login_page/login_fm.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:logger/logger.dart';
 
@@ -34,7 +36,6 @@ class SessionGVM extends Notifier<SessionModel> {
     Map<String, dynamic> body =
         await UserRepository().join(username, email, password); // 나중에는 싱글톤으로 관리하기 <- 성공 실패 상관없이 다 들어온다
 
-    // 통신 실패
     if (!body["success"]) {
       // 토스트 띄우기
       ScaffoldMessenger.of(mContext).showSnackBar(
@@ -47,7 +48,47 @@ class SessionGVM extends Notifier<SessionModel> {
     Navigator.pushNamed(mContext, "/login"); // 기존 컨텍스트를 알아야함
   }
 
-  Future<void> login(String username, String email) async {}
+  Future<void> login(String username, String password) async {
+    // 1. 유효성 검사
+    Logger().d("username : ${username}, password : ${password}");
+    bool isValid = ref.read(loginProvider.notifier).validate(); // 최종 유효성 검사
+    if (!isValid) {
+      ScaffoldMessenger.of(mContext).showSnackBar(
+        SnackBar(content: Text("유효성 검사 실패입니다.")),
+      );
+      return; // 유효성 검사 실패의 경우 아무것도 하지 않도록
+    }
+
+    // 2. 통신
+    Map<String, dynamic> body = await UserRepository().login(username, password);
+    if (!body["success"]) {
+      // 토스트 띄우기
+      ScaffoldMessenger.of(mContext).showSnackBar(
+        SnackBar(content: Text("${body["errorMessage"]}")),
+      );
+      return; // 유효성 검사 실패 시 아무것도 하지 않도록
+    }
+
+    // 3. Token을 디바이스에 저장 (앱을 껐다 켰을 때 디바이스에 저장 되있으면 토큰 확인 후 자동 로그인 가능)
+    // 비동기함수이기 때문에 기다리다가 내려가도록 await
+    await secureStorage.write(key: "accessToken", value: body["response"]["accessToken"]);
+
+    // 4. 세션 모델 갱신
+    // 나중에는 fromMap으로 파싱하는 걸 만들어야함
+    state = SessionModel(
+        id: body["response"]["id"],
+        username: body["response"]["username"],
+        imgUrl: body["response"]["imgUrl"],
+        accessToken: body["response"]["accessToken"],
+        isLogin: true);
+
+    // 5. Dio의 header에 토큰 세팅
+    dio.options.headers["Authorization"] = body["response"]["accessToken"];
+
+    // 6. 게시글 목록 페이지로 이동
+    // 화면 날리고 게시글 목록 페이지로 이동한다
+    Navigator.pushNamed(mContext, "/post/list");
+  }
 
   Future<void> logout() async {}
 }
@@ -56,12 +97,14 @@ class SessionGVM extends Notifier<SessionModel> {
 class SessionModel {
   int? id;
   String? username;
+  String? imgUrl;
   String? accessToken;
   bool? isLogin;
 
   SessionModel(
       {this.id,
       this.username,
+      this.imgUrl,
       this.accessToken,
       this.isLogin = false}); // 초기화될 때 id, username, accessToken은 null, isLogin은 false
 }
--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -19,4 +19,28 @@ class UserRepository {
     Logger().d(responseBody);
     return responseBody; // responseBody 안에 정보가 다 있다
   }
+
+  Future<Map<String, dynamic>> login(String username, String password) async {
+    // 1. Map 변환
+    final requestBody = {
+      "username": username,
+      "password": password,
+    };
+
+    // 2. 통신
+    Response response = await dio.post("/login", data: requestBody); // Response<T> 타입
+    Map<String, dynamic> responseBody = response.data; // final 사용 안 하고 타입 정확하게 명시하기
+    Logger().d(responseBody);
+
+    // 3. Header에서 Token 꺼내기 (body만 보내므로 / record 사용하면 Header랑 같이 보낼 수 있음)
+    String accessToken = "";
+    try {
+      accessToken = response.headers["Authorization"]![0]; // null이 절대 아니다
+      responseBody["response"]["accessToken"] = accessToken;
+    } catch (e) {
+      // null이면 accessToken은 비어있음
+    }
+    Logger().d(responseBody);
+    return responseBody; // responseBody 안에 정보가 다 있다
+  }
 }
